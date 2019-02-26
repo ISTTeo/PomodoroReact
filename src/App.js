cf9180cefeafeeed6f6dccf3ce16092ea5744c42
Teo <ldptek@gmail.com>
2019-02-26 18:37:19 +0000

    Add Folder to Organize Timer Functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import TimerApp from './TimerApp'
+import TimerApp from './Timer/TimerApp'
 class App extends Component {
 
 
--- src/Timer/TimerApp.js
+++ src/Timer/TimerApp.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import StartButton from './StartButton'
 import TimerInput from './TimerInput'
